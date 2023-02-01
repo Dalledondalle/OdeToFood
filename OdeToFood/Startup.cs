dde7e5737a324cb2448eca7af1501d26d9cb06b9
Daniel Kåre Kristensen <dani9241@elev.techcollege.dk>
2023-02-01 17:29:49 +0100

    Add project files.

---
+++ OdeToFood/Startup.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OdeToFood.Data;
+
+namespace OdeToFood
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddDbContextPool<OdeToFoodDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("OdeToFoodDb"));
+            });
+
+            services.AddScoped<IRestaurantData, SqlRestaurantData>();
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, 
+                              IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.Use(SayHelloMiddleware);
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseNodeModules(env);
+            app.UseCookiePolicy();
+            
+            app.UseMvc();
+        }
+
+        private RequestDelegate SayHelloMiddleware(
+                                    RequestDelegate next)
+        {
+            return async ctx =>
+            {
+
+                if (ctx.Request.Path.StartsWithSegments("/hello"))
+                {
+                    await ctx.Response.WriteAsync("Hello, World!");
+                }
+                else
+                {
+                    await next(ctx);                    
+                }
+            };
+        }
+    }
+}
